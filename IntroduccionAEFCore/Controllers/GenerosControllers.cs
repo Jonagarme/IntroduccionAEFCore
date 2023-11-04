4fc7ffb87bf5b7f248e49767b4b8139cbfc062f3
jonagarme <garciajonathan268@gmail.com>
2023-11-04 18:29:32 -0500

    se agrego el generosController y el dto de genero

---
+++ IntroduccionAEFCore/Controllers/GenerosControllers.cs
@@ -0,0 +1,30 @@
+﻿using IntroduccionAEFCore.DTOs;
+using IntroduccionAEFCore.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroduccionAEFCore.Controllers
+{
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosControllers: ControllerBase
+    {
+        private readonly ApplicactionDBContext context;
+
+        public GenerosControllers(ApplicactionDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>Post(GeneroCreacionDTO generoCreacion)
+        {
+            var genero = new Genero
+            {
+                Nombre = generoCreacion.Nombre,
+            };
+            context.Add(genero);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
