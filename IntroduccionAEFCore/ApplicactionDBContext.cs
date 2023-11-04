9c22d7d1b8b2b53d6317a9e56900e9a9af6ab02f
jonagarme <garciajonathan268@gmail.com>
2023-11-04 16:43:55 -0500

    Entidad Genero creada, con sus respectivas migraciones

--- IntroduccionAEFCore/ApplicactionDBContext.cs
+++ IntroduccionAEFCore/ApplicactionDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IntroduccionAEFCore.Entidades;
+using Microsoft.EntityFrameworkCore;
 
 namespace IntroduccionAEFCore
 {
@@ -7,5 +8,15 @@ namespace IntroduccionAEFCore
         public ApplicactionDBContext(DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genero>().HasKey(g => g.Id);
+            modelBuilder.Entity<Genero>().Property(g => g.Nombre).HasMaxLength(150);
+        }
+
+        public DbSet<Genero> Generos => Set<Genero>();
     }
 }
