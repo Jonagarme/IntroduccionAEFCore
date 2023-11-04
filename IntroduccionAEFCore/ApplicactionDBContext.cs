169f1e3641f9df4292d489abaa6ffd5f981d75ec
jonagarme <garciajonathan268@gmail.com>
2023-11-04 14:17:33 -0500

    Agregar archivos de proyecto.

---
+++ IntroduccionAEFCore/ApplicactionDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionAEFCore
+{
+    public class ApplicactionDBContext : DbContext
+    {
+        public ApplicactionDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
